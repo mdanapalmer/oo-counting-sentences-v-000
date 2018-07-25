f667a49d36fc95cb78eedd9c938c5cdcb40073aa
Dana Palmer <roiai4@hotmail.com>
2018-07-25 07:03:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
+  #The count_sentences method needs to be explained.
+  #self.end_with?("!")
+  #self.split.length solves the first two tests...
+  #binding.pry
 end
